c7ece09fa9040393b67a60614023a04dcc9f1a27
yub168 <76650700+yub168@users.noreply.github.com>
2023-03-29 22:33:10 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,7 +9,25 @@
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
-#添加autotimeset
-#git clone https://github.com/sirpdboy/luci-app-autotimeset package/luci-app-autotimeset
+#修改 Linux 内核版本
+sed -i 's/KERNEL_PATCHVER:=5.15/KERNEL_PATCHVER:=6.1/g' ./target/linux/x86/Makefile
+
+#删除默认安装包
+sed -i 's/luci-app-adbyby-plus //g' ./target/linux/x86/Makefile
+sed -i 's/lluci-app-ipsec-vpnd //g' ./target/linux/x86/Makefile
+sed -i 's/luci-app-unblockmusic //g' ./target/linux/x86/Makefile
+sed -i 's/luci-app-zerotier //g' ./target/linux/x86/Makefile
+sed -i 's/luci-app-xlnetacc //g' ./target/linux/x86/Makefile
+sed -i 's/luci-app-wireguard //g' ./target/linux/x86/Makefile
+sed -i 's/luci-app-nlbwmon //g' ./include/target.mk
+sed -i 's/luci-app-wol //g' include/target.mk
+#sed -i 's/luci-app-nlbwmon //g'
+
+#添加安装包
+#sed -i 's/kmod-drm-amdgpu/& store/' ./target/linux/x86/Makefile
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+
+#修改活动连接数
+# 16384 是原来的连接数，65535 是要修改成的连接数
+sed -i 's/16384/65535/g' package/kernel/linux/files/sysctl-nf-conntrack.conf
