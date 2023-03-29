6e1c16eac129964f24a26d75baa0db17ec523493
yub168 <76650700+yub168@users.noreply.github.com>
2023-03-29 19:10:19 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,4 +12,4 @@
 #添加autotimeset
 #git clone https://github.com/sirpdboy/luci-app-autotimeset package/luci-app-autotimeset
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
