f7fdb9e04a5aeb6cca43b46b9dc6443d469a0c84
yub168 <76650700+yub168@users.noreply.github.com>
2023-03-30 07:54:26 +0000

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -25,9 +25,15 @@ sed -i 's/luci-app-wol //g' include/target.mk
 
 #添加安装包
 #sed -i 's/kmod-drm-amdgpu/& store/' ./target/linux/x86/Makefile
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+
+# 修改lan口地址
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
 #修改活动连接数
 # 16384 是原来的连接数，65535 是要修改成的连接数
 sed -i 's/16384/65535/g' package/kernel/linux/files/sysctl-nf-conntrack.conf
+
+#修改wan口为pppoe 增加拨号账号与密码
+sed -i 's/2:-dhcp/2:-pppoe/g' package/base-files/files/lib/functions/uci-defaults.sh
+sed -i 's/username=\'username\'/username=\'yub168\'/g' package/base-files/files/bin/config_generate
+sed -i 's/password=\'password\'/password=\'yb790913\'/g' package/base-files/files/bin/config_generate
